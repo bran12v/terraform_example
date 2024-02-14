e573c8a1f6eb5e3d455bf5c69238d795736b0bb1
bran12v <brandon.a.vanek@gmail.com>
2024-02-14 11:13:21 -0500

    first

---
+++ vpc.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "5.36.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "1.123.234.21/22"
+}
